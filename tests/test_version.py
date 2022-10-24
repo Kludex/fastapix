52de7d5eeaa7d123c610d1a009884b8ea4215f0f
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-10-24 21:50:20 +0200

    💚 Fix release pipeline (#12)

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,4 +8,4 @@ runner = CliRunner()
 def test_version() -> None:
     result = runner.invoke(app, ["--version"])
     assert result.exit_code == 0
-    assert "FastAPI X 0.1.0" in result.stdout
+    assert "FastAPI X" in result.stdout
